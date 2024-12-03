3b9d4af9e82869c59ec52980fe633451615e5745
ROCKET 247 <58811361+Delmas-code@users.noreply.github.com>
2024-12-03 09:10:34 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const PORT = process.env.VITE_REACT_PORT
+// https://vitejs.dev/config/
+/*
 export default defineConfig({
   plugins: [react()],
+})
+  */
+
+//for server purposes
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: PORT
+  }
 })
